2a2ee832417b196d81cebc4bd59b6087f6c66cf6
Mervan YALÇIN <mervan.yalcin@st.uskudar.edu.tr>
2022-06-01 21:11:50 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -31,3 +31,12 @@ function App() {
 }
 
 export default App;
+
+// git init
+// git add .
+// git status
+// git commit -m 'first commit'
+// git branch -M main
+// git remote add origin https://github.com/your-project-directly-link */
+// git push
+// git push --set-upstream origin main
